893465498d41d3558d6488106958fa7cefdcbf56
weslley bulhoes <weslley.bulhoes@gmail.com>
2021-04-21 18:47:32 -0300

    Iniciando

---
+++ BACIA/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for BACIA project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BACIA.settings')
+
+application = get_wsgi_application()
